5d02bd57af1c0995f083de0c58749c5fd2d2f060
anku99999 <anandankit776@gmail.com>
2025-08-21 22:09:33 +0530

    my first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages , setup
+
+
+
+
+setup(
+    name = "scrapper",
+    version = '0.0.1',
+    author = 'ankit',
+    author_email = 'anandankit776@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
